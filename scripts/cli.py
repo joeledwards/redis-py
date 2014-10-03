0706429842a035d2e6f26445fa578ce0f04a3c12
Joel Edwards <joeledwards@gmail.com>
2014-10-03 14:19:47 -0600

    Added new 'ttl' command for checking the TTL (time to live) of keys which have an expiration set

--- scripts/cli.py
+++ scripts/cli.py
@@ -230,6 +230,7 @@ class CommandLine: #/*{{{*/
         self.__add_action(Action("set", self.set).with_num_args(2).with_usage("<key> <value> - set string entry"))
         self.__add_action(Action("show", self.show).with_min_args(1).with_max_args(2).with_usage("<key> [field] - print value of this entry"))
         self.__add_action(Action("time", self.time).with_num_args(1).with_usage("<timestamp> - format the given timestamp"))
+        self.__add_action(Action("ttl", self.ttl).with_num_args(1).with_usage("<key> - return the TTL (time to live) in seconds for this key"))
 
     def __add_action(self, action):
         self.actions[action.name] = action
@@ -491,6 +492,14 @@ class CommandLine: #/*{{{*/
         for key in path_map.keys():
             map_summary += "  [%s] PATH> %d\n" % (key, path_map[key])
         return map_summary + self.db.display_entries(keys)
+
+    def ttl(self, key):
+        ttl_seconds = self.db.redis().ttl(key)
+        if ttl_seconds is None or ttl_seconds == -1:
+            return "key '%s' is not set to expire" % key
+        if ttl_seconds == -2:
+            return "key '%s' doesn't exist" % key
+        return "TTL for '%s' is %d s" % (key, ttl_seconds)
 
 #/*}}}*/
 
